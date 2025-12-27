cdf4e5a30573a175568089fb2d812d519b0abe97
anurag1302 <anurag3487@gmail.com>
2025-12-27 22:00:20 +0530

    Payments Controller - simulate an unreliable payment system

---
+++ payment-service/PaymentService.Sln/PaymentService/Controllers/PaymentsController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using PaymentService.Models;
+
+namespace PaymentService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PaymentsController:ControllerBase
+{
+    private static readonly Random random = new Random();
+
+    [HttpPost()]
+    public async Task<IActionResult> ProcessPayment([FromBody] ProcessPayment model)
+    {
+        var randomNumber = random.Next(1, 10);
+        
+        //Simulate a faulty payment system, at times it has some delay, at times it fails
+        if (randomNumber <= 5)
+        {
+            await Task.Delay(3000);
+        }
+
+        if (randomNumber <= 3)
+        {
+            return StatusCode(500, "Payment Gateway Failure");
+        }
+
+        return Ok(new
+        {
+            Id = Guid.NewGuid(),
+            Message = "Payment Successful"
+        });
+    }
+}
---
+++ payment-service/PaymentService.Sln/PaymentService/Models/ProcessPayment.cs
@@ -0,0 +1,7 @@
+namespace PaymentService.Models;
+
+public class ProcessPayment
+{
+    public Guid Id { get; set; }
+    public string Status { get; set; }
+}
