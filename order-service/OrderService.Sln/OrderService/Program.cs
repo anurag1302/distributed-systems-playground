2a6b31a692b2751c4c6c219c12b72d0a7e68e661
anurag1302 <anurag3487@gmail.com>
2025-12-28 00:50:02 +0530

    add resilience in the order service via polly

--- order-service/OrderService.Sln/OrderService/Program.cs
+++ order-service/OrderService.Sln/OrderService/Program.cs
@@ -1,9 +1,29 @@
+using Polly;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient();
+// builder.Services.AddHttpClient("payments", client =>
+// {
+//     client.Timeout = TimeSpan.FromSeconds(2);
+// });
+
+
+builder.Services.AddHttpClient("payments", client =>
+    {
+        client.Timeout = TimeSpan.FromSeconds(2);
+    })
+    .AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync(3,
+        retryAttempt =>
+        {
+            Console.WriteLine($"Retry attempt - {retryAttempt}");
+            return TimeSpan.FromMilliseconds(200);
+        }))
+    .AddTransientHttpErrorPolicy(policy => policy.CircuitBreakerAsync(5, 
+        TimeSpan.FromSeconds(30)));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
